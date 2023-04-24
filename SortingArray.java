7e7ff924d4e79d7b4636ce8574031942734fd873
ndt4m <131661538+ndt4m@users.noreply.github.com>
2023-04-24 19:05:40 +0700

    Add files via upload

---
+++ SortingArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SortingArray 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the size of array: ");
+        int n = input.nextInt();
+        
+        int sum = 0;
+        int[] arr = new int[n];
+        System.out.println("Begin to fill the array!");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = input.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+        double avg = (double) sum / n;
+
+        for (int i : arr) 
+        {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nThe sum of the array is: " + sum);
+        System.out.println("The avarage of the array is: " + avg);
+
+    }
+}
