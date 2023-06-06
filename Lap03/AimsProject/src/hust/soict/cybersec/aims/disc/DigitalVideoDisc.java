7c7cde22c5e4cfeb92883a5705225cd50714d6c4
ndt4m <131661538+ndt4m@users.noreply.github.com>
2023-06-06 23:59:45 +0700

    Add files via upload
    finish lap03

---
+++ Lap03/AimsProject/src/hust/soict/cybersec/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,97 @@
+package hust.soict.cybersec.aims.disc;
+
+public class DigitalVideoDisc 
+{
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getCategory()
+    {
+        return this.category;
+    }
+
+    public String getDirector()
+    {
+        return this.director;   
+    }
+
+    public int getLength()
+    {
+        return this.length;
+    }
+
+    public float getCost()
+    {
+        return this.cost;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public DigitalVideoDisc()
+    {
+        DigitalVideoDisc.nbDigitalVideoDiscs += 1;
+        this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title)
+    {
+        this();
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost)
+    {
+        this();
+        this.category = category;
+        this.cost = cost;
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String tilte, String category, String director, float cost)
+    {
+        this();
+        this.title = tilte;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost)
+    {
+        this();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public boolean isMatch(String title) 
+    {
+		return this.getTitle().equals(title);
+	}
+
+    @Override
+    public String toString()
+    {
+        return "DVD - " + "[" + this.title + "] - [" + this.director + "] - [" + this.length + "]: [" + this.cost + "]$";
+    }
+}
---
+++ Lap03/AimsProject/src/hust/soict/cybersec/aims/store/Store.java
@@ -0,0 +1,44 @@
+package hust.soict.cybersec.aims.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
+public class Store 
+{
+    private List<DigitalVideoDisc> itemsInStore;
+
+    public Store() {
+        itemsInStore = new ArrayList<DigitalVideoDisc>();
+    }
+
+    public void addDVD(DigitalVideoDisc... dvdList) 
+    {
+        if (dvdList.length == 0)
+        {
+            System.out.println("The dvd List is empty");
+            return;
+        }
+        
+        for (DigitalVideoDisc dvd : dvdList)
+        {
+            this.itemsInStore.add(dvd);
+        }
+        System.out.println("DVDs have added successfully");
+    }
+
+    public void removeDVD(DigitalVideoDisc... dvdList) 
+    {
+        if (dvdList.length == 0)
+        {
+            System.out.println("The dvd List is empty");
+            return;
+        }
+
+        for (DigitalVideoDisc dvd : dvdList)
+        {
+            this.itemsInStore.remove(dvd);
+        }
+        System.out.println("DVDs have removed successfully");
+    }
+}
